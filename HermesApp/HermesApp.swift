b129d25b4f77b7e2f991f56dfa8d4c198770ef0c
tthanhannguyen-a11y <tthanhannguyen@gmail.com>
2026-07-23 12:18:48 +0700

    Add HermesApp/HermesApp.swift

---
+++ HermesApp/HermesApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct HermesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .preferredColorScheme(nil)
+        }
+    }
+}
